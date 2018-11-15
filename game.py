d41b0fb856bb99229652d8e55ab64c96485a9e55
Danzkiz <mszpirt@gmail.com>
2018-11-15 18:17:20 +0100

    Implementation of minimax ai

--- game.py
+++ game.py
@@ -186,5 +186,109 @@ def random_coordinates(input_board):
 
     return [x_coord, y_coord]
 
+
+def available_moves(board):
+    legal_moves = []
+    # find all legal moves
+    for x in range(len(board)):
+        for y in range(len(board[x])):
+            if board[x][y] == '_':
+                legal_moves.append([x, y])
+
+    return legal_moves
+
+
+def minimax_recursion(board, game_turn, path, moves, shortest_path):
+    solutions = []
+
+    # test each possible move left on 'board'
+    for move in moves:
+
+        # make a test move
+        place_token(get_current_player(game_turn), board, int(move[0]), int(move[1]))
+
+        # remember move
+        path.append(move)
+
+        remaining_moves = available_moves(board)
+
+        # check to see if move wins or tie the game
+        if did_win('X', board):
+            solutions.append({"score": -10, "turns": game_turn, "path": list(path)})
+            board[path[-1][0]][path[-1][1]] = '_'
+            path.pop()
+            if shortest_path[0] > game_turn:
+                shortest_path[0] = game_turn
+
+            return solutions
+
+        if did_win('O', board):
+            solutions.append({"score": 10, "turns": game_turn, "path": list(path)})
+            board[path[-1][0]][path[-1][1]] = '_'
+            path.pop()
+            if shortest_path[0] > game_turn:
+                shortest_path[0] = game_turn
+
+            return solutions
+
+        if len(remaining_moves) == 0:
+            solutions.append({"score": 0, "turns": game_turn, "path": list(path)})
+            board[path[-1][0]][path[-1][1]] = '_'
+            path.pop()
+
+            return solutions
+
+        else:
+            # checks to see if it's a better solution than before
+            if shortest_path[0] >= game_turn:
+                solutions.extend(minimax_recursion(board, game_turn + 1, path, remaining_moves, shortest_path))
+            # removes the test move
+            board[path[-1][0]][path[-1][1]] = '_'
+            path.pop()
+
+    return solutions
+
+
+def find_minimax_solutions(game_turn, board):
+    avail_moves = available_moves(board)
+
+    # find solutions
+    return minimax_recursion(board, game_turn, [], avail_moves, possible_turns=[len(board) ** 2])
+
+
+def ai_coordinates(game_turn, board):
+    computer_move = []
+    possible_solutions = find_minimax_solutions(game_turn, board)
+
+    sorted_solutions = sorted(possible_solutions, key=lambda x: x['turns'])
+
+    # who is min_player and who is max_player
+    if get_current_player(game_turn) == 'X':
+        win_solutions = filter(lambda x: x['score'] != 10, sorted_solutions)
+        loose_solutions = filter(lambda x: x['score'] != -10, sorted_solutions)
+    else:
+        win_solutions = filter(lambda x: x['score'] != -10, sorted_solutions)
+        loose_solutions = filter(lambda x: x['score'] != 10, sorted_solutions)
+
+    # can I win next round?
+    for solution in win_solutions:
+        if len(solution['path']) == 1:
+            computer_move = solution['path'][0]
+            break
+
+    # do I loose next round?
+    for solution in loose_solutions:
+        if len(solution['path']) == 2:
+            computer_move = solution['path'][1]
+            break
+
+    # the not so smart choice:
+    if len(computer_move) == 0:
+        computer_move = win_solutions[random.randint(0, len(win_solutions))]['path'][0]
+
+    print("Computer: I pick " + "[" + str(computer_move[0] + 1) + "," + str(computer_move[1] + 1) + "]")
+    return computer_move
+
+
 # Run the program
 game_loop()
